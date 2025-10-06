c6ca75266ad73f817d2a1ff526f7ad9cf619e9cc
Laura-Oliveira <laura.regina.cp@gmail.com>
2025-10-05 22:14:37 -0300

    [FrontEnd] Implementing new logic to show country flags on the dropdownmenu and on the current time result

--- composeApp/src/commonMain/kotlin/com/kmp/time/view/Time.kt
+++ composeApp/src/commonMain/kotlin/com/kmp/time/view/Time.kt
@@ -1,5 +1,6 @@
 package com.kmp.time.view
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -9,7 +10,10 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeContentPadding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
@@ -23,17 +27,26 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import kmplocaltime.composeapp.generated.resources.Res
+import kmplocaltime.composeapp.generated.resources.eg
+import kmplocaltime.composeapp.generated.resources.fr
+import kmplocaltime.composeapp.generated.resources.jp
+import kmplocaltime.composeapp.generated.resources.mx
 import kotlinx.datetime.LocalTime
 import kotlinx.datetime.TimeZone
 import kotlinx.datetime.toLocalDateTime
+import org.jetbrains.compose.resources.imageResource
+import org.jetbrains.compose.resources.painterResource
 import kotlin.time.Clock
 import kotlin.time.ExperimentalTime
 
-data class Country(val name: String, val zone: TimeZone)
+data class Country(val name: String, val zone: TimeZone, val image: Painter)
 
 @OptIn(ExperimentalTime::class)
 fun currentTimeAt(location: String, zone: TimeZone): String {
@@ -52,12 +65,14 @@ fun currentTimeAt(location: String, zone: TimeZone): String {
     return "$hour:$minute:$second"
 }
 
+
+@Composable
 fun countries() = listOf(
-    Country("Japan", TimeZone.of("Asia/Tokyo")),
-    Country("France", TimeZone.of("Europe/Paris")),
-    Country("Mexico", TimeZone.of("America/Mexico_City")),
-    Country("Indonesia", TimeZone.of("Asia/Jakarta")),
-    Country("Egypt", TimeZone.of("Africa/Cairo")),
+    Country("Japan", TimeZone.of("Asia/Tokyo"), painterResource(Res.drawable.jp)),
+    Country("France", TimeZone.of("Europe/Paris"), painterResource(Res.drawable.fr)),
+    Country("Mexico", TimeZone.of("America/Mexico_City"), painterResource(Res.drawable.mx)),
+    Country("Indonesia", TimeZone.of("Asia/Jakarta"), painterResource(Res.drawable.jp)),
+    Country("Egypt", TimeZone.of("Africa/Cairo"), painterResource(Res.drawable.eg)),
 )
 
 @Composable
@@ -89,11 +104,24 @@ fun Time() {
                     ) {
                         countries().forEach { country ->
                             DropdownMenuItem(
-                                text = { Text(country.name) },
+//                                text = { Text(country.name) },
                                 onClick = {
-                                    selectedCountry = Country(country.name, country.zone)
+                                    selectedCountry = Country(country.name, country.zone, country.image)
                                     timeAtLocation = currentTimeAt(country.name, country.zone)
                                     showCountries = false
+                                },
+                                text = {
+                                    Row (verticalAlignment = Alignment.CenterVertically){
+                                        Image(
+                                            painter = country.image,
+                                            contentDescription = "country flag",
+                                            modifier = Modifier
+                                                .size(24.dp)
+                                        )
+                                        Spacer(modifier = Modifier.width(8.dp))
+
+                                        Text(country.name)
+                                    }
                                 }
                             )
                         }//list
@@ -108,22 +136,57 @@ fun Time() {
                     Text("Select Location")
                 }//Button
 
-                Text(
-                    text = when {
-                        selectedCountry != null -> "${selectedCountry?.name}: $timeAtLocation"
-                        else -> "No Location Selected"
-                                },
+                //Time
+                if(selectedCountry != null)
+                {
+                    Row(verticalAlignment = Alignment.CenterVertically,
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .wrapContentSize(Alignment.Center))
+                    {
+                        Image(
+                            painter = selectedCountry!!.image,
+                            contentDescription = "country flag",
+                            modifier = Modifier.size(24.dp)
+                        )
 
-//                    if(timeAtLocation != "No location selected")
-//                    { "Current Time $timeAtLocation" }
-//                    else
-//                    { timeAtLocation },
-                    style = TextStyle(fontSize = 20.sp),
-                    textAlign = TextAlign.Center,
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .wrapContentSize(Alignment.Center)
-                )//Time
+                        Spacer(modifier = Modifier.width(8.dp))
+
+                        Text(
+                            text = "${selectedCountry!!.name}: $timeAtLocation",
+                            style = TextStyle(fontSize = 20.sp),
+                            textAlign = TextAlign.Center
+                        )
+                    }
+                }
+                else
+                {
+                    Text(
+                        text = "No Location Selected",
+                        style = TextStyle(fontSize = 20.sp),
+                        textAlign = TextAlign.Center,
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .wrapContentSize(Alignment.Center)
+                    )
+                }
+
+//                Text(
+////                    text = when {
+////                        selectedCountry != null -> "${selectedCountry?.image} ${selectedCountry?.name}: $timeAtLocation"
+////                        else -> "No Location Selected"
+////                                },
+//
+////                    if(timeAtLocation != "No location selected")
+////                    { "Current Time $timeAtLocation" }
+////                    else
+////                    { timeAtLocation },
+//                    style = TextStyle(fontSize = 20.sp),
+//                    textAlign = TextAlign.Center,
+//                    modifier = Modifier
+//                        .fillMaxSize()
+//                        .wrapContentSize(Alignment.Center)
+//                )//Time
             }//Column
         }//Box
     }//MaterialTheme
